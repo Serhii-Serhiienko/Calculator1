0e2f81c88a7820089f848483fca9aa6893677115
Serhii-Serhiienko <71976003+Serhii-Serhiienko@users.noreply.github.com>
2021-06-04 14:04:19 +0300

    Add files via upload

---
+++ Calculator_MVP/Calculator_MVP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_MVP
+{
+    static class Program
+    {
+    
+        [STAThread]
+        static void Main()
+        {
+      
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Model model = new Model();
+            View1 view1 = new View1();
+            Presenter p = new Presenter(model, view1);
+            Application.Run(view1);
+        }
+    }
+}
